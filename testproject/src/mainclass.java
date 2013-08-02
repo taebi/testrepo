cad3af0ee8cebb84e68e9a9e81ae83ce63bfcb72
taebi <tobias.aebi@u-s-p.ch>
2013-08-02 16:06:05 +0200

    second test to commit

--- testproject/src/mainclass.java
+++ testproject/src/mainclass.java
@@ -21,6 +21,7 @@ public class mainclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		//change comment to test gitrepo
 	}
 
 }

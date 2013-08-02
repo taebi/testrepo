25515722bd5d3992814d88807bf91da0bf057d9e
taebi <tobias.aebi@gox.ch>
2013-08-02 11:54:59 +0200

    inittest

---
+++ testproject/src/mainclass.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author taebi
+ *
+ */
+public class mainclass {
+
+	/**
+	 * 
+	 */
+	public mainclass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
